da9a69a071cf749e28dd6ec1840b84680fc91480
TtomasSzx <donizetetomas07@gmail.com>
2024-11-06 21:02:56 -0500

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
+import { Button } from "./_components/ui/button"; 
+
 const Home = () => {
   return (
-    <div className="itens-center flex w-screen justify-center">
-      <h1 className="p-5 text-red-500">Hello</h1>
-    </div>
+    <Button>Hello</Button>
   );
 };
 
