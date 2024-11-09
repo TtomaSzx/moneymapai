0b1927f6f5968b3525c2df7a6b5555094f8e3187
TtomasSzx <donizetetomas07@gmail.com>
2024-11-08 21:56:56 -0500

    chore: config index.tsx

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -4,6 +4,28 @@ import { Transaction } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import TransactionTypeBadge from "../_components/type-badge";
 
+export const TRANSACTION_CATEGORY_LABELS = {
+  EDUCATION: "Educação",
+  ENTERTAINMENT: "Entretenimento",
+  FOOD: "Alimentação",
+  HEALTH: "Saúde",
+  HOUSING: "Moradia",
+  OTHER: "Outros",
+  SALARY: "Salário",
+  TRANSPORTATION: "Transporte",
+  UTILITY: "Utilidades",
+};
+
+export const TRANSACTION_PAYMENT_METHOD_LABELS = {
+  BANK_TRANSFER: "Transferência bancária",
+  BANK_SLIP: "Boleto bancário",
+  CASH: "Dinheiro",
+  CREDIT_CARD: "Cartão de crédito",
+  DEBIT_CARD: "Cartão de débito",
+  OTHER: "Outros",
+  PIX: "Pix",
+};
+
 export const transactionscolumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "name",
@@ -19,18 +41,14 @@ export const transactionscolumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "category",
     header: "Categoria",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_CATEGORY_LABELS[transaction.category],
   },
   {
     accessorKey: "paymentMethod",
     header: "Método de Pagamento",
-  },
-  {
-    accessorKey: "category",
-    header: "Categoria",
-  },
-  {
-    accessorKey: "paymentMethod",
-    header: "Método de pagamento",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_PAYMENT_METHOD_LABELS[transaction.paymentMethod],
   },
   {
     accessorKey: "date",
